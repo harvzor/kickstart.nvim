c566170ce7bfba93dc7f70c94c10c3c8664e408c
harvzor <harveyjwilliams@aol.co.uk>
2025-08-14 13:55:18 +0200

    Make shift-tab untab.

--- init.lua
+++ init.lua
@@ -205,6 +205,11 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- [[ Custom keymap ]]
+
+-- set shift-tab to remove the tabbing
+vim.keymap.set('i', '<S-Tab>', '<C-d>')
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
